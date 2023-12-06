678946a1c47bae92e03db873d86a3fe2a241c99d
primesh-patel <primesh.patel.ce.2015@miet.ac.in.com>
2023-12-06 15:53:08 +0530

    add sidebar

--- dashboard.py
+++ dashboard.py
@@ -18,3 +18,33 @@ if fl is not None:
 else:
     os.chdir(r"D:\campusX\ds_1")
     df = pd.read_csv("SampleSuperstore.csv", encoding = "ISO-8859-1")
+
+col1, col2 = st.columns((2))
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+# Gettting the min and max date
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+st.sidebar.header("Choose your filter: ")
+# Create for Region
+region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+    # Create for State
+state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]
